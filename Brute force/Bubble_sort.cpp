607c63a77c4b8b233d6a7ae50e0cced4513691b9
Ronnakorn Hompoa <36933333+kasettakorn@users.noreply.github.com>
2022-02-23 10:16:01 +0700

    Create Bubble_sort.cpp

---
+++ Brute force/Bubble_sort.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+void printArray(lli arr[], lli n) {
+    for(lli i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubble_sort(lli arr[], lli n) {
+    for(lli i=0; i<n; i++) {
+        printArray(arr, n);
+        for(lli j=0; j<n-i-1; j++) {
+            if(arr[j+1] < arr[j]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+    lli arr[10000], n=0;
+    while(true) {
+        string str;
+        cin >> str;
+
+        if(str == "b") {
+            bubble_sort(arr, n);
+            break;
+        }
+        else {
+            arr[n] = stoi(str);
+            n++;
+        }
+
+    }
+    return 0;
+}
+
