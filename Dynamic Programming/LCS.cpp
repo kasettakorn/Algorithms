3d97b2984954aa25af0ef1279892d511d5d7194b
Ronnakorn Hompoa <36933333+kasettakorn@users.noreply.github.com>
2022-03-28 12:50:37 +0700

    Update LCS.cpp

--- Dynamic Programming/LCS.cpp
+++ Dynamic Programming/LCS.cpp
@@ -8,7 +8,7 @@ void LCS(char x[], char y[]) {
      for(int i=0; i<=strlen(x); i++) {
         for(int j=0; j<=strlen(y); j++) {
             if(i == 0 || j == 0) dp[i][j] = 0;
-            else if (x[i] == y[j]) {
+            else if (x[i-1] == y[j-1]) {
                 dp[i][j] = 1 + dp[i-1][j-1];
             }
             else {
